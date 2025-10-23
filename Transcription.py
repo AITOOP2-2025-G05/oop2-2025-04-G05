5d44cf34436d8946d3d8571b45ed5a796bdb7e1f
Alicia4771 <rinn1204@icloud.com>
2025-10-23 15:29:44 +0900

    変更追加

--- Transcription.py
+++ Transcription.py
@@ -1,3 +1,5 @@
+import os
+os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = "1"
 import mlx_whisper
 from pydub import AudioSegment
 import numpy as np
@@ -5,10 +7,6 @@ import numpy as np
 # 音声ファイルを指定して文字起こし
 audio_file_path = "python-audio-output.wav"
 
-# result = mlx_whisper.transcribe(
-#   audio_file_path, path_or_hf_repo="whisper-base-mlx"
-# )
-# print(result)
 result = mlx_whisper.transcribe(
     audio_file_path,
     path_or_hf_repo="mlx-community/whisper-base-mlx"  # ←これが正
